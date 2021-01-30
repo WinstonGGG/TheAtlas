f81c055f46790d6653685e7ae366fc86aa76ccb9
Zhengyang Zhang <zzhan127@ucsc.edu>
2021-01-29 18:41:50 -0800

    camera initial function added

--- TheAtlasRebirth/Assets/Scripts/PlayerCamera.cs
+++ TheAtlasRebirth/Assets/Scripts/PlayerCamera.cs
@@ -6,10 +6,12 @@ public class PlayerCamera : MonoBehaviour
 {
     // Start is called before the first frame update
 	private Vector3 dy;
+	bool init = true;
 	//private Vector3 dz;
     void Start()
     {
-        dy = new Vector3(0f,-0.4f,0.5f);
+        dy = new Vector3(0f,-1f,0.5f);
+		
 		//dz = (0,0,-0.3f);
     }
 
@@ -18,6 +20,14 @@ public class PlayerCamera : MonoBehaviour
     {
 		float cameraY = this.transform.localPosition.y;
 		float cameraZ = this.transform.localPosition.z;
+		if(init){
+			if(cameraZ>= -40f){
+				init = false;
+			}
+			Vector3 offset = new Vector3(0f,-0.1f,0.05f);
+			this.transform.position += offset;
+			
+		}
         if(Input.GetAxis("Mouse ScrollWheel")>0f &&cameraZ <= -30f ){
 			this.transform.position += dy;
 			// Debug.Log(cameraY);
@@ -28,4 +38,6 @@ public class PlayerCamera : MonoBehaviour
 			// Debug.Log(cameraY);
 		}
     }
+	
+	
 }
