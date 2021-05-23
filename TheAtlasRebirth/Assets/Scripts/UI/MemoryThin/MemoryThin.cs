faba6a78a7aa31d1ba7b7a7f54815eddb6ead362
Zminghao <Zminghao@dragonhao.github.com>
2021-05-23 12:03:08 +0800

    MemoryThin first
    记忆薄新文件

---
+++ TheAtlasRebirth/Assets/Scripts/UI/MemoryThin/MemoryThin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryThin : MonoBehaviour
+{
+    [HideInInspector]
+    private int length; //存在的记忆数量
+
+    public struct ItemInfo
+    {
+        public enum MemorySort { image, text, imageText };
+        public MemorySort memorySort;
+        public Sprite memoryIcon;
+        public string memoryText;
+        public string memoryTime;
+    }
+
+    public ItemInfo[] MemoryThinInfos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        length = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
