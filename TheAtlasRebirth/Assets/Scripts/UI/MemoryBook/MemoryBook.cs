4750dc6b647a9f7adf2672628d3ffe5303feb513
Zminghao <Zminghao@dragonhao.github.com>
2021-05-30 15:36:49 +0800

    修改文件名字，增加图片
    修改文件名字，增加图片

--- TheAtlasRebirth/Assets/Scripts/UI/MemoryBook/MemoryBook.cs
+++ TheAtlasRebirth/Assets/Scripts/UI/MemoryBook/MemoryBook.cs
@@ -1,27 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MemoryThin : MonoBehaviour
+public class MemoryBook : MonoBehaviour
 {
-    private int length; //存在的记忆数量
+    private int length;  //存在的记忆数量
+
+    private int memoryPage;  //记忆的页数
 
     [System.Serializable]
     public struct ItemInfo
     {
         public enum MemorySort { image, text, imageText };
-        public MemorySort memorySort;
-        public Sprite memoryIcon;
+        public MemorySort memorySort;  //记忆种类
+        public Sprite memoryIcon;  //
         public string memoryText;
         public string memoryTime;
     }
 
-    public ItemInfo[] MemoryThinInfos;
+    public ItemInfo[] memoryBookInfos;
 
     // Start is called before the first frame update
     void Start()
     {
         length = 0;
+        length = memoryBookInfos.Length;
+        memoryPage = (int)Math.Ceiling((double)length / 3);
     }
 
     // Update is called once per frame
