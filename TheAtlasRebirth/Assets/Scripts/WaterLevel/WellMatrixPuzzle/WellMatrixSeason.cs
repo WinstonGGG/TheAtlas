0b2a6603ce37ef165de31a1f16d8188606f410f1
Liuwenda (Winston) Gao <lgao9@ucsc.edu>
2021-07-11 11:49:59 -0700

    wellmatrix puzzle season reposition while enlarge

--- TheAtlasRebirth/Assets/Scripts/WaterLevel/WellMatrixPuzzle/WellMatrixSeason.cs
+++ TheAtlasRebirth/Assets/Scripts/WaterLevel/WellMatrixPuzzle/WellMatrixSeason.cs
@@ -5,16 +5,41 @@ using UnityEngine;
 public class WellMatrixSeason : MonoBehaviour
 {
     private bool canEnlarge = true;
+    private string season;
+    private RectTransform rect;
+
+    void Start() {
+        season = gameObject.name;
+        rect = gameObject.GetComponent<RectTransform>();
+    }
     public void Enlarge() {
         if (canEnlarge) {
-            transform.localScale *= 2;
+            transform.localScale *= 1.5f;
+            if (season.CompareTo("Spring") == 0) {
+                rect.anchoredPosition = rect.anchoredPosition - new Vector2(25, 0);
+            } else if (season.CompareTo("Fall") == 0) {
+                rect.anchoredPosition = rect.anchoredPosition + new Vector2(25, 0);
+            } else if (season.CompareTo("Summer") == 0) {
+                rect.anchoredPosition = rect.anchoredPosition + new Vector2(0, 25);
+            } else {
+                rect.anchoredPosition = rect.anchoredPosition - new Vector2(0, 25);
+            }
             canEnlarge = false;
         }
     }
 
     public void Shrink() {
         if (!canEnlarge) {
-            transform.localScale /= 2;
+            transform.localScale /= 1.5f;
+            if (season.CompareTo("Spring") == 0) {
+                rect.anchoredPosition = rect.anchoredPosition + new Vector2(25, 0);
+            } else if (season.CompareTo("Fall") == 0) {
+                rect.anchoredPosition = rect.anchoredPosition - new Vector2(25, 0);
+            } else if (season.CompareTo("Summer") == 0) {
+                rect.anchoredPosition = rect.anchoredPosition - new Vector2(0, 25);
+            } else {
+                rect.anchoredPosition = rect.anchoredPosition + new Vector2(0, 25);
+            }
             canEnlarge = true;
         }
     }
